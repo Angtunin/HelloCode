ea74178e248b3ba1c6a8a77155d5933e46f162e9
AleksandrTunin <angtunin@mail.ru>
2022-09-23 15:35:00 +0800

    add Lecture4

---
+++ Lecture4/Example013/Program.cs
@@ -0,0 +1,30 @@
+﻿// Работа с двумерным строковым массивом
+
+string[,] table = new string[2, 5];
+table[0, 0] = "слово";
+table[0, 1] = "слово";
+table[0, 4] = "слово";
+table[1, 0] = "слово";
+table[1, 2] = "слово";
+table[1, 4] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.Write($"-{table[rows, columns]}-");
+//     }
+// Console.WriteLine();
+// }
+
+// числовой массив
+int[,] matrix = new int[3, 4];
+
+for (int rows = 0; rows < 3; rows++) // для указания row < 3 м. использовать row < matrix.GetLength(0) 0 для строк, 1 для стобцов
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        Console.Write($"{matrix[rows, columns]} ");
+    }
+    Console.WriteLine();
+}
---
+++ Lecture4/Example013_3_Fackt/Program.cs
@@ -0,0 +1,25 @@
+﻿// метод для решения факториала
+
+int Factorial(int n) // замена типа int на double может отсрочить переполнение типа
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+// Console.WriteLine(Factorial(5));
+
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+double Factorial1(int n) // замена типа int на double может отсрочить переполнение типа
+{
+    if (n == 1) return 1;
+    else return n * Factorial1(n - 1);
+}
+
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial1(i)}");
+}
